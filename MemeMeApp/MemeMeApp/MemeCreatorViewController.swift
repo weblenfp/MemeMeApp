da12b0e4b8b73c7293d4ddbc7f083f547da63d46
Weblen Fernandes Pimentel <>
2021-07-01 01:56:50 -0300

    Including Impact font style into the project.

--- MemeMeApp/MemeMeApp/MemeCreatorViewController.swift
+++ MemeMeApp/MemeMeApp/MemeCreatorViewController.swift
@@ -21,7 +21,7 @@ class MemeCreatorViewController: UIViewController, UIImagePickerControllerDelega
     var memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.font: UIFont(name: "Impact", size: 40)!,
         NSAttributedString.Key.strokeWidth: -5
     ]
     
